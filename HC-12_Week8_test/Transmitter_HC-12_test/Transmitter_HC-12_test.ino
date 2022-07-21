4e9e8b2d3d491b03c5cfd68955b24e33347d3851
Joey <joeyrolfe@hotmail.com.au>
2022-07-21 12:22:36 +1000

    Preparing Transmitter Side for Week8 Demonstration

--- HC-12_Week8_test/Transmitter_HC-12_test/Transmitter_HC-12_test.ino
+++ HC-12_Week8_test/Transmitter_HC-12_test/Transmitter_HC-12_test.ino
@@ -15,8 +15,8 @@ SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(19200);             // Serial port to computer
-  HC12.begin(19200);               // Serial port to HC12
+  Serial.begin(9600);             // Serial port to computer
+  HC12.begin(9600 );               // Serial port to HC12
   while(dht20.begin()){
     Serial.println("Initialize sensor failed");
     delay(1000);
@@ -32,22 +32,22 @@ void loop() {
   }
 
    A3_Read = analogRead(LMT86);
-   voltage = (A3_Read*5)/1024;
+   voltage = (A3_Read*5)/1024;    // ( Analog_Reading * Power Supply Voltage ) / (ADC resolution)  --- Analog to Digital Conversion (10 bit ADC)
 
-  float Temperature = (10.888 - sqrt(143.217468-13.88*voltage))/(-0.00694) + 30;
+  float Temperature = (10.888 - sqrt(143.217468-13.88*voltage))/(-0.00694) + 30;    // Equation to transform into Degrees Celsius from datasheet
+  
   String StrTemp = String(Temperature, 2);
   String StrDHT20T = String(dht20.getTemperature(), 2);
-  Serial.print("LMT86T: "); Serial.print(StrTemp[0]); Serial.print(StrTemp[1]); 
-  Serial.print(" || LMT Voltage: "); Serial.print(voltage); 
-  Serial.print(" || DHT20: "); Serial.print(dht20.getTemperature());
-  Serial.print(" || DHT20String: "); Serial.println(StrDHT20T);
   
-  if(Temperature >= 25){
+  Serial.print("LMT86T: "); Serial.print(StrTemp[0]); Serial.print(StrTemp[1]); 
+  Serial.print(" || DHT20: "); Serial.println(dht20.getTemperature());
+  
+  if(Temperature >= 25){        // If Temperature is > 25 Degrees Celsius, then turn on LED
       digitalWrite(13, HIGH);
   } else {
       digitalWrite(13, LOW);
   }
-  HC12.write(StrTemp[0]);      // Send that data to HC-12
-  HC12.write(StrTemp[1]);
+  HC12.write(StrTemp[0]);      // Transmit the LMT Temperature Readings
+  HC12.write(StrTemp[1]);      
   delay(1000); 
   }

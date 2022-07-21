7ca2d8d32d34ed79be34eed9af426912823f4bc2
JoeyMacOS <joeyrolfe@hotmail.com.au>
2022-07-21 12:21:21 +1000

    Preparing for Week8 Demonstration

---
+++ HC-12_Week8_test/Receiver_HC-12_test/Receiver_HC-12_test.ino
@@ -0,0 +1,45 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+char tempChar;
+String LMT86Temperature;
+String NewLMT86Temperature;
+
+void setup() {
+  Serial.begin(9600);             // Serial port to computer
+  HC12.begin(9600);               // Serial port to HC12
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  while (HC12.available()) {        // If HC-12 has data (Receiving) 
+    
+     tempChar = HC12.read();                  // tempChar is collecting each character that has been received
+     
+     if(LMT86Temperature.length() < 2){       // Concatening tempChar into a string so that it is readable.
+     LMT86Temperature += tempChar;
+     } else {
+      
+       NewLMT86Temperature += tempChar;
+       
+       if(NewLMT86Temperature != LMT86Temperature){
+        
+         LMT86Temperature = NewLMT86Temperature;
+         
+         NewLMT86Temperature = "";
+       }
+     }
+
+    if(LMT86Temperature.length() == 2){       
+      if(LMT86Temperature.toInt() >= 25){       // Turn on LED is temperature is above 25 degrees
+      digitalWrite(13, HIGH);
+      } else {
+      digitalWrite(13, LOW);
+      }
+    }
+  }
+  while (Serial.available()) {      // If Serial monitor has data (Transmitting)
+    HC12.write(Serial.read());      // Send that data to HC-12
+  }
+
+ }
